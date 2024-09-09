eec2ace1a4b82e246237beec99d8b261188e4233
smitsharma-420 <smit.insignia@gmail.com>
2024-09-09 18:09:38 +0530

    Path Module Added

---
+++ PathModule/pathmod.js
@@ -0,0 +1,14 @@
+const path = require("path");
+
+console.log(
+    path.dirname("C:UsersadminDesktop/node_js_trainning/PathModule/pathmod.js")
+);
+console.log(
+    path.extname("C:UsersadminDesktop/node_js_trainning/PathModule/pathmod.js")
+);
+console.log(
+    path.basename("C:UsersadminDesktop/node_js_trainning/PathModule/pathmod.js")
+);  
+console.log(
+  path.parse("C:UsersadminDesktop/node_js_trainning/PathModule/pathmod.js")
+);

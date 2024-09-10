11e82a40b89770e30f85a0981efad9caa9630016
smitsharma-420 <smit.insignia@gmail.com>
2024-09-10 10:25:57 +0530

    import export module added

---
+++ importExport/index.js
@@ -0,0 +1,7 @@
+const {add, sub, mult, name} = require("./oper")
+
+// console.log(add(5, 5));
+console.log(add(10, 5));
+console.log(sub(10, 5));
+console.log(mult(10, 5));
+console.log(name);
---
+++ importExport/oper.js
@@ -0,0 +1,19 @@
+const add = (a, b) => {
+    return a + b;
+};
+
+const sub = (a, b) => {
+    return a - b;
+};
+
+const mult = (a, b) => {
+    return a * b;
+};
+
+const name = "Sharma";
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.mult = mult;
+
+module.exports = {add, sub, mult, name};

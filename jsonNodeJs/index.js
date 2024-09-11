c70956dc414517813dd8951a55c2c67cafaea395
smitsharma-420 <smit.insignia@gmail.com>
2024-09-11 12:48:46 +0530

    Json Module added

---
+++ jsonNodeJs/index.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+
+const biodata = {
+    name: "Smit",
+    surname: "Sharma",
+    age: 21,
+    bloodgroup: "O+",
+}
+
+// create Json data    
+const jsonData = JSON.stringify(biodata);
+
+// push in the new file
+fs.writeFile("json1.json", jsonData, (err) => {
+    console.log("Done ✅");
+})
+
+// read the data of another file 
+fs.readFile("json1.json", "utf-8", (err, data) => {
+// Make it original data
+    const orgData = JSON.parse(data);
+    console.log(data);
+    console.log(orgData);
+})
